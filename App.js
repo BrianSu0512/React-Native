2a6e446b2859d592b525538428a795e05e3f3510
46184597 <weichia.su@students.mq.edu.au>
2022-04-12 11:21:34 +1000

    "MEMORIES" app

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AccountScreen from './APP/AppScreen/AccountScreen';
+import LoginScreen from './APP/AppScreen/LoginScreen';
+import MemoriesScreen from './APP/AppScreen/MemoriesScreen';
+import MoreInfScreen from './APP/AppScreen/MoreInfScreen';
+import RegisterScreen from './APP/AppScreen/RegisterScreen';
+import NewPostsScreen from './APP/AppScreen/NewPostsScreen';
+import WelcomeScreen from './APP/AppScreen/WelcomeScreen';
+
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from './APP/Navigation/AppNavigator';
+
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+  <AppNavigator />
+  </NavigationContainer>
+
+
+  
+
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
